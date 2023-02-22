c33261aadb77b75e7ea3107a24844023fb4a10bb
regular-meet-8 <eddiekim444@gmail.com>
2023-02-22 09:58:30 +0300

    ruby-00-inheritance-lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
 
-end
+
+# class Student 
+#     attr_accessor :first_name, :last_name
+
+#   def initialize
+#     @first_name = ""
+#     @last_name = ""
+#   end
+# end
+
+
+
+
+class Student < User
+    attr_accessor :knowledge
+  
+    def initialize
+      super
+      @knowledge = []
+    end
+  
+    def learn(string)
+      @knowledge << string
+    end
+  end
+  Footer
+  
